fbdaa0ad1a06a703386d147a19df8ad1d09d1d45
Kamesh-Mishra <kamesh.mishra20@gmail.com>
2021-11-27 14:12:24 +0530

    Added New Files

---
+++ 01.Hello_World.cs
@@ -0,0 +1,70 @@
+﻿/* in C# we can write hello world program in multiple ways, here is some example 
+
+>> Simple Example
+
+>> using System
+
+>> Using public modifier
+
+>>Using namespace
+
+*/
+
+
+
+// C# Simple Example
+
+/*
+
+class Helloworld
+{
+    static void Main(String[] args)
+    {
+        System.Console.WriteLine("Hello, World!!" +
+                           "\nThis is C#");
+    }
+}
+
+*/
+
+/*
+// using System
+using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!!");
+        }
+
+    }
+*/
+
+/*
+//using public modifier
+
+using System;
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        Console.WriteLine("Hello World!!");
+        }
+    }
+*/
+
+//using namespace
+
+using System;
+
+namespace MyNamespace
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!!");
+        }
+    }
+}
