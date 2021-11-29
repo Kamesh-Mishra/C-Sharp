56087bd9f68bdde4643e704171c6391314f5d28f
Kamesh-Mishra <kamesh.mishra20@gmail.com>
2021-11-29 12:56:56 +0530

    Switch in C#

---
+++ 03.Switch.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class SwitchExample
+{
+    public static void Main(string[] args)
+        {
+            Console.Write("Enter a number :");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            switch (number)
+                {
+                case 11: Console.WriteLine("It is eleven");
+                    break;
+                case 22: Console.WriteLine("It is twenty two");
+                    break;
+                case 33: Console.WriteLine("It is thirty three");
+                    break ;
+                default: Console.WriteLine("Your Number is " + number);
+                    break;
+                 }
+       }
+}
