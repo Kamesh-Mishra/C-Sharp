8b836f54bc6bf83f696e4129c0dabcfeeb83d60c
Kamesh-Mishra <kamesh.mishra20@gmail.com>
2021-12-19 17:29:55 +0530

    Added new file

---
+++ projects/LoginForm/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace LoginForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection conn = new SqlConnection(@"Data Source=DESKTOP-D1T721P\SQLEXPRESS;Initial Catalog=loginform;Integrated Security=True");
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_login_Click(object sender, EventArgs e)
+        {
+            string username, user_password;
+
+            username = txt_username.Text;
+            user_password = txt_password.Text;
+
+            try
+            {
+                string querry = "SELECT * FROM Login_new WHERE username = '" + txt_username.Text + "'AND password = '" + txt_password.Text + "'";
+                SqlDataAdapter sda = new SqlDataAdapter(querry, conn);
+
+                DataTable dtable = new DataTable();
+                sda.Fill(dtable);
+
+                if(dtable.Rows.Count > 0)
+                {
+                    username = txt_username.Text;
+                    user_password = txt_password.Text; 
+
+                    // page that needed to be load next
+                    Menuform form2 = new Menuform();
+                    form2.Show();
+                    this.Hide();
+                }
+                else
+                {
+                    MessageBox.Show("Invalid Login Details","Error", MessageBoxButtons.OK , MessageBoxIcon.Error);
+                    txt_username.Clear();
+                    txt_password.Clear();
+
+                    //to focus username
+                    txt_username.Focus();
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Error");
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        private void button_clear_Click(object sender, EventArgs e)
+        {
+            txt_username.Clear();
+            txt_password.Clear();
+
+            txt_username.Focus();
+
+        }
+    }
+}
---
+++ projects/testADO/testADO/Program.cs
@@ -0,0 +1,137 @@
+﻿
+using System;
+using System.Data.SqlClient;
+
+
+namespace Test
+{
+    class Program
+    {
+        readonly string ConnectionString = @"Data Source=DESKTOP-D1T721P\SQLEXPRESS;Initial Catalog=test;Integrated Security=True";
+        public void Table()
+        {
+            SqlConnection conn = new SqlConnection();
+
+            
+            try
+            {
+                conn = new SqlConnection(@"Data Source=DESKTOP-D1T721P\SQLEXPRESS;Initial Catalog=test;Integrated Security=True");
+                //conn.ConnectionString = "Data Source=DESKTOP-D1T721P\SQLEXPRESS;Initial Catalog=test;Integrated Security=True"
+                conn.Open();
+                SqlCommand Command = new SqlCommand("CREATE TABLE TestTable(id int not null, name varchar(50), email varchar(50))", conn);                
+                Command.ExecuteNonQuery();
+                
+
+                Console.WriteLine("Table Done");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error" + e.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+        }
+
+        public void Insert()
+        {
+            SqlConnection conn;
+
+            try
+            {
+                conn = new SqlConnection(@"Data Source=DESKTOP-D1T721P\SQLEXPRESS;Initial Catalog=test;Integrated Security=True");
+                //conn.ConnectionString = "Data Source=DESKTOP-D1T721P\SQLEXPRESS;Initial Catalog=test;Integrated Security=True"
+                conn.Open();
+
+                SqlCommand Command = new SqlCommand(@"INSERT INTO TestTable (id, name, email) VALUES ('102', 'kamesh', 'abc@ghi.com')", conn);
+                int rowsAffected = Command.ExecuteNonQuery();
+                Console.WriteLine("Inserted Rows = " + rowsAffected);
+                conn.Close();
+                Console.WriteLine("Data inserted!!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error" + ex.Message);
+            }
+
+
+        }
+
+        public void Fetch()
+        {
+            SqlConnection conn;
+            try
+            {
+                conn = new SqlConnection(@"Data Source=DESKTOP-D1T721P\SQLEXPRESS;Initial Catalog=test;Integrated Security=True");
+                //conn.ConnectionString = "Data Source=DESKTOP-D1T721P\SQLEXPRESS;Initial Catalog=test;Integrated Security=True"
+                conn.Open();
+
+                SqlCommand Command = new SqlCommand("SELECT * FROM TestTable", conn);
+
+
+
+                SqlDataReader DataString = Command.ExecuteReader();      // DataReader read
+                Console.WriteLine(DataString.HasRows);
+
+                while (DataString.Read())      // Iterating Data using loop  
+                {
+                      
+                    Console.WriteLine(DataString["id"] + " " + DataString["name"] + " " + DataString["email"]);       // show data
+                    //Console.WriteLine(DataString[0] + " " + DataString[1] + " " + DataString[2]);
+                }
+                conn.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error" + e.Message);
+            }
+        }
+
+        public void Delete()
+        {
+            //SqlConnection conn = new SqlConnection();
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(ConnectionString))
+                {
+                    SqlCommand Command = new SqlCommand("DELETE FROM TestTable WHERE id = '101'", conn);
+                    conn.Open();
+                    Command.ExecuteNonQuery();
+                    Console.WriteLine("Data Deleted!");
+                }
+                 /*   conn = new SqlConnection(ConnectionString);
+                //conn.ConnectionString = "Data Source=DESKTOP-D1T721P\SQLEXPRESS;Initial Catalog=test;Integrated Security=True"
+                conn.Open();
+
+                SqlCommand Command = new SqlCommand("DELETE FROM TestTable WHERE id = '101'", conn);
+
+                Command.ExecuteNonQuery();
+                Console.WriteLine("Data Deleted!");
+
+                */
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("ERROR" + e.Message);
+            }/*
+            finally
+            {
+                conn.Close();
+            }*/
+        }
+
+
+        static void Main()
+        {
+            Program p = new Program();
+            //p.Table();
+            //p.Insert();
+            p.Fetch();
+            //p.Delete();
+        }
+
+    }
+}
