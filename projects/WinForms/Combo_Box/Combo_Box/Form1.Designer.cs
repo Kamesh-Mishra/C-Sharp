0e93dbb7e9c02f2666db458cc8dd6172d9a94dc1
Kamesh-Mishra <kamesh.mishra20@gmail.com>
2021-12-27 16:51:28 +0530

    Added new files

---
+++ projects/WinForms/Combo_Box/Combo_Box/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace Combo_Box
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.HobbiesComboBox = new System.Windows.Forms.ComboBox();
+            this.AddButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(139, 111);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Hobbies";
+            // 
+            // Label2
+            // 
+            this.Label2.AutoSize = true;
+            this.Label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label2.Location = new System.Drawing.Point(170, 50);
+            this.Label2.Name = "Label2";
+            this.Label2.Size = new System.Drawing.Size(51, 25);
+            this.Label2.TabIndex = 2;
+            this.Label2.Text = "Add";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(286, 41);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(489, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // HobbiesComboBox
+            // 
+            this.HobbiesComboBox.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.HobbiesComboBox.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.HobbiesComboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HobbiesComboBox.FormattingEnabled = true;
+            this.HobbiesComboBox.Items.AddRange(new object[] {
+            "WRITING",
+            "READING",
+            "SINGING",
+            "DANCING",
+            "LISTENING",
+            "SWIMMING",
+            "CRICKET"});
+            this.HobbiesComboBox.Location = new System.Drawing.Point(286, 111);
+            this.HobbiesComboBox.Name = "HobbiesComboBox";
+            this.HobbiesComboBox.Size = new System.Drawing.Size(489, 33);
+            this.HobbiesComboBox.TabIndex = 4;
+            this.HobbiesComboBox.SelectedIndexChanged += new System.EventHandler(this.HobbiesComboBox_SelectedIndexChanged);
+            // 
+            // AddButton
+            // 
+            this.AddButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AddButton.Location = new System.Drawing.Point(816, 39);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(110, 36);
+            this.AddButton.TabIndex = 5;
+            this.AddButton.Text = "Add";
+            this.AddButton.UseVisualStyleBackColor = true;
+            this.AddButton.Click += new System.EventHandler(this.AddButton_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(286, 166);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(489, 37);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Count ComboBox Items";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(286, 220);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(489, 34);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Remove Specific Item";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(286, 271);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(489, 34);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Clear All Items of ComboBox";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(286, 321);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(489, 33);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Sort Items";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(282, 374);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 20);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "label3";
+            this.label3.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(948, 428);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.AddButton);
+            this.Controls.Add(this.HobbiesComboBox);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Combo Box Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox HobbiesComboBox;
+        private System.Windows.Forms.Button AddButton;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ projects/WinForms/Combo_Box/Combo_Box/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Combo_Box
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text != "")
+            {
+                if(HobbiesComboBox.Items.Contains(textBox1.Text))
+                {
+                    MessageBox.Show("Items Already Exists");
+                }
+                else
+                {
+                    HobbiesComboBox.Items.Add(textBox1.Text);
+                    textBox1.Clear();
+                    textBox1.Focus();
+
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("Please Fill The TextBox");
+            }
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Total Items are " + HobbiesComboBox.Items.Count);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //HobbiesComboBox.Items.Remove(HobbiesComboBox.SelectedItem);
+            HobbiesComboBox.Items.RemoveAt(HobbiesComboBox.SelectedIndex);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            HobbiesComboBox.Items.Clear();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            HobbiesComboBox.Sorted = true;
+        }
+
+        private void HobbiesComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            label3.Text = "Your Selected item is : "+ HobbiesComboBox.SelectedItem.ToString();
+            label3.Visible = true;
+        }
+    }
+}
