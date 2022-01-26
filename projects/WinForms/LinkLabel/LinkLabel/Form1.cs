b65c88a6cb4163435b685f9c4b896fac8c343a39
Kamesh-Mishra <kamesh.mishra20@gmail.com>
2022-01-26 16:55:03 +0530

    Added new file

---
+++ projects/WinForms/LinkLabel/LinkLabel/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace LinkLabel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Form2 f2 = new Form2();
+            f2.Show();
+
+            //Process.Start("http://www.google.com");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.linkLabel1.LinkBehavior = System.Windows.Forms.LinkBehavior.NeverUnderline;
+        }
+    }
+}
