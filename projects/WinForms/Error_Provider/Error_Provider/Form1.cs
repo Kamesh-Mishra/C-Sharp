6db600a98149d5dcc384711b7bf3d4e4f4639589
Kamesh-Mishra <kamesh.mishra20@gmail.com>
2022-02-10 18:04:57 +0530

    Added new file

---
+++ projects/WinForms/Error_Provider/Error_Provider/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Error_Provider
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox1.Text))
+            {
+                textBox1.Focus();
+                errorProvider1.SetError(this.textBox1, "Please fill the name!!");
+                //MessageBox.Show("Please fill the name !!");
+            }
+            else
+            {
+                errorProvider1.Clear();
+            }
+        }
+
+        private void textBox2_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox2.Text))
+            {
+                textBox2.Focus();
+                errorProvider2.SetError(this.textBox2, "Please fill the age!!");
+                //MessageBox.Show("Please fill the Age !!");
+            }
+            else
+            {
+                errorProvider2.Clear();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox1.Text))
+            {
+                textBox1.Focus();
+                errorProvider1.SetError(this.textBox1, "Please fill the name!!");
+                //MessageBox.Show("Please fill the name !!");
+            }
+
+            else if (string.IsNullOrEmpty(textBox2.Text))
+            {
+                textBox2.Focus();
+                errorProvider2.SetError(this.textBox2, "Please fill the age!!");
+                //MessageBox.Show("Please fill the Age !!");
+            }
+            else
+            {
+                MessageBox.Show("Welcome !!");
+            }
+            
+        }
+    }
+}
