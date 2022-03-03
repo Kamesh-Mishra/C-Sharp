ec58bef8b676cc88c6ea461a11bb2ddfed1e9a87
Kamesh-Mishra <kamesh.mishra20@gmail.com>
2022-03-03 10:39:02 +0530

    Added new file

---
+++ projects/userlogin/userlogin/Login.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace userlogin
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.signin_btn = new System.Windows.Forms.Button();
+            this.signup_btn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_username = new System.Windows.Forms.TextBox();
+            this.txt_password = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // signin_btn
+            // 
+            this.signin_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.signin_btn.Location = new System.Drawing.Point(157, 177);
+            this.signin_btn.Name = "signin_btn";
+            this.signin_btn.Size = new System.Drawing.Size(107, 44);
+            this.signin_btn.TabIndex = 0;
+            this.signin_btn.Text = "SIGN IN";
+            this.signin_btn.UseVisualStyleBackColor = true;
+            this.signin_btn.Click += new System.EventHandler(this.signin_btn_Click);
+            // 
+            // signup_btn
+            // 
+            this.signup_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.signup_btn.Location = new System.Drawing.Point(318, 177);
+            this.signup_btn.Name = "signup_btn";
+            this.signup_btn.Size = new System.Drawing.Size(111, 44);
+            this.signup_btn.TabIndex = 1;
+            this.signup_btn.Text = "SIGN UP";
+            this.signup_btn.UseVisualStyleBackColor = true;
+            this.signup_btn.Click += new System.EventHandler(this.signup_btn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(111, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "USERNAME :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(111, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "PASSWORD :";
+            // 
+            // txt_username
+            // 
+            this.txt_username.Location = new System.Drawing.Point(233, 49);
+            this.txt_username.Name = "txt_username";
+            this.txt_username.Size = new System.Drawing.Size(196, 22);
+            this.txt_username.TabIndex = 4;
+            // 
+            // txt_password
+            // 
+            this.txt_password.Location = new System.Drawing.Point(233, 108);
+            this.txt_password.Name = "txt_password";
+            this.txt_password.Size = new System.Drawing.Size(196, 22);
+            this.txt_password.TabIndex = 5;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(585, 281);
+            this.Controls.Add(this.txt_password);
+            this.Controls.Add(this.txt_username);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.signup_btn);
+            this.Controls.Add(this.signin_btn);
+            this.Name = "Login";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button signin_btn;
+        private System.Windows.Forms.Button signup_btn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_username;
+        private System.Windows.Forms.TextBox txt_password;
+    }
+}
+
