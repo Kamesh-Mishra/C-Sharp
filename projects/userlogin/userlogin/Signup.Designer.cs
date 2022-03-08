2dae1cb81d905857e83ea0d9f3bef4fc107738ee
Kamesh-Mishra <kamesh.mishra20@gmail.com>
2022-03-08 10:41:10 +0530

    Added new file

---
+++ projects/userlogin/userlogin/Signup.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace userlogin
+{
+    partial class Signup
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txt_name_in = new System.Windows.Forms.TextBox();
+            this.txt_email_in = new System.Windows.Forms.TextBox();
+            this.txt_username_in = new System.Windows.Forms.TextBox();
+            this.txt_password_in = new System.Windows.Forms.TextBox();
+            this.txt_phone_in = new System.Windows.Forms.TextBox();
+            this.txt_city_in = new System.Windows.Forms.TextBox();
+            this.signup_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(186, 53);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(186, 109);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Email :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(186, 163);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(106, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Username :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(186, 221);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(103, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Password :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(186, 279);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(73, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Phone :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(186, 337);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(54, 20);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "City :";
+            // 
+            // txt_name_in
+            // 
+            this.txt_name_in.Location = new System.Drawing.Point(318, 50);
+            this.txt_name_in.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txt_name_in.Name = "txt_name_in";
+            this.txt_name_in.Size = new System.Drawing.Size(316, 27);
+            this.txt_name_in.TabIndex = 6;
+            // 
+            // txt_email_in
+            // 
+            this.txt_email_in.Location = new System.Drawing.Point(318, 106);
+            this.txt_email_in.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txt_email_in.Name = "txt_email_in";
+            this.txt_email_in.Size = new System.Drawing.Size(316, 27);
+            this.txt_email_in.TabIndex = 7;
+            // 
+            // txt_username_in
+            // 
+            this.txt_username_in.Location = new System.Drawing.Point(318, 160);
+            this.txt_username_in.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txt_username_in.Name = "txt_username_in";
+            this.txt_username_in.Size = new System.Drawing.Size(316, 27);
+            this.txt_username_in.TabIndex = 8;
+            // 
+            // txt_password_in
+            // 
+            this.txt_password_in.Location = new System.Drawing.Point(318, 215);
+            this.txt_password_in.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txt_password_in.Name = "txt_password_in";
+            this.txt_password_in.Size = new System.Drawing.Size(316, 27);
+            this.txt_password_in.TabIndex = 9;
+            // 
+            // txt_phone_in
+            // 
+            this.txt_phone_in.Location = new System.Drawing.Point(318, 276);
+            this.txt_phone_in.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txt_phone_in.Name = "txt_phone_in";
+            this.txt_phone_in.Size = new System.Drawing.Size(316, 27);
+            this.txt_phone_in.TabIndex = 10;
+            // 
+            // txt_city_in
+            // 
+            this.txt_city_in.Location = new System.Drawing.Point(318, 334);
+            this.txt_city_in.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txt_city_in.Name = "txt_city_in";
+            this.txt_city_in.Size = new System.Drawing.Size(316, 27);
+            this.txt_city_in.TabIndex = 11;
+            // 
+            // signup_button
+            // 
+            this.signup_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.signup_button.Location = new System.Drawing.Point(318, 415);
+            this.signup_button.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.signup_button.Name = "signup_button";
+            this.signup_button.Size = new System.Drawing.Size(318, 54);
+            this.signup_button.TabIndex = 12;
+            this.signup_button.Text = "Signup";
+            this.signup_button.UseVisualStyleBackColor = true;
+            // 
+            // Signup
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(852, 534);
+            this.Controls.Add(this.signup_button);
+            this.Controls.Add(this.txt_city_in);
+            this.Controls.Add(this.txt_phone_in);
+            this.Controls.Add(this.txt_password_in);
+            this.Controls.Add(this.txt_username_in);
+            this.Controls.Add(this.txt_email_in);
+            this.Controls.Add(this.txt_name_in);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Signup";
+            this.Text = "Signup";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txt_name_in;
+        private System.Windows.Forms.TextBox txt_email_in;
+        private System.Windows.Forms.TextBox txt_username_in;
+        private System.Windows.Forms.TextBox txt_password_in;
+        private System.Windows.Forms.TextBox txt_phone_in;
+        private System.Windows.Forms.TextBox txt_city_in;
+        private System.Windows.Forms.Button signup_button;
+    }
+}
