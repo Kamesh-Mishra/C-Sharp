a2351fc7c911f02ba6c938db92c760b1721f001e
Kamesh-Mishra <kamesh.mishra20@gmail.com>
2022-03-12 00:25:36 +0530

    Added new file

---
+++ projects/userlogin/userlogin/User_Details.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace userlogin
+{
+    public partial class User_Details : Form
+    {
+        public User_Details()
+        {
+            InitializeComponent();
+        }
+
+        private void User_Details_Load(object sender, EventArgs e)
+        {
+            Output_Name.Text = "DEMO_NAME";
+            Output_Email.Text = "Demo email";
+            Output_Phone.Text = "demo phone";
+            Output_City.Text = "demo city";
+        }
+    }
+}
