e12d40c0cea6e3ab3b14af3705be5b4ffd5e2313
Kamesh-Mishra <kamesh.mishra20@gmail.com>
2022-03-11 22:23:53 +0530

    Added new file

---
+++ projects/userlogin/userlogin/User_Details.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace userlogin
+{
+    partial class User_Details
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Label_Name = new System.Windows.Forms.Label();
+            this.Label_Email = new System.Windows.Forms.Label();
+            this.Label_Phone = new System.Windows.Forms.Label();
+            this.Label_City = new System.Windows.Forms.Label();
+            this.Output_Name = new System.Windows.Forms.Label();
+            this.Output_Email = new System.Windows.Forms.Label();
+            this.Output_Phone = new System.Windows.Forms.Label();
+            this.Output_City = new System.Windows.Forms.Label();
+            this.edit_btn = new System.Windows.Forms.Button();
+            this.signout_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Label_Name
+            // 
+            this.Label_Name.AutoSize = true;
+            this.Label_Name.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label_Name.Location = new System.Drawing.Point(162, 40);
+            this.Label_Name.Name = "Label_Name";
+            this.Label_Name.Size = new System.Drawing.Size(81, 25);
+            this.Label_Name.TabIndex = 0;
+            this.Label_Name.Text = "Name :";
+            // 
+            // Label_Email
+            // 
+            this.Label_Email.AutoSize = true;
+            this.Label_Email.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label_Email.Location = new System.Drawing.Point(162, 91);
+            this.Label_Email.Name = "Label_Email";
+            this.Label_Email.Size = new System.Drawing.Size(78, 25);
+            this.Label_Email.TabIndex = 1;
+            this.Label_Email.Text = "Email :";
+            // 
+            // Label_Phone
+            // 
+            this.Label_Phone.AutoSize = true;
+            this.Label_Phone.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label_Phone.Location = new System.Drawing.Point(162, 145);
+            this.Label_Phone.Name = "Label_Phone";
+            this.Label_Phone.Size = new System.Drawing.Size(87, 25);
+            this.Label_Phone.TabIndex = 2;
+            this.Label_Phone.Text = "Phone :";
+            // 
+            // Label_City
+            // 
+            this.Label_City.AutoSize = true;
+            this.Label_City.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label_City.Location = new System.Drawing.Point(162, 194);
+            this.Label_City.Name = "Label_City";
+            this.Label_City.Size = new System.Drawing.Size(63, 25);
+            this.Label_City.TabIndex = 3;
+            this.Label_City.Text = "City :";
+            // 
+            // Output_Name
+            // 
+            this.Output_Name.AutoSize = true;
+            this.Output_Name.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Output_Name.Location = new System.Drawing.Point(295, 42);
+            this.Output_Name.Name = "Output_Name";
+            this.Output_Name.Size = new System.Drawing.Size(0, 22);
+            this.Output_Name.TabIndex = 4;
+            // 
+            // Output_Email
+            // 
+            this.Output_Email.AutoSize = true;
+            this.Output_Email.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Output_Email.Location = new System.Drawing.Point(295, 93);
+            this.Output_Email.Name = "Output_Email";
+            this.Output_Email.Size = new System.Drawing.Size(0, 22);
+            this.Output_Email.TabIndex = 5;
+            // 
+            // Output_Phone
+            // 
+            this.Output_Phone.AutoSize = true;
+            this.Output_Phone.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Output_Phone.Location = new System.Drawing.Point(295, 147);
+            this.Output_Phone.Name = "Output_Phone";
+            this.Output_Phone.Size = new System.Drawing.Size(0, 22);
+            this.Output_Phone.TabIndex = 6;
+            // 
+            // Output_City
+            // 
+            this.Output_City.AutoSize = true;
+            this.Output_City.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Output_City.Location = new System.Drawing.Point(295, 196);
+            this.Output_City.Name = "Output_City";
+            this.Output_City.Size = new System.Drawing.Size(0, 22);
+            this.Output_City.TabIndex = 7;
+            // 
+            // edit_btn
+            // 
+            this.edit_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.edit_btn.Location = new System.Drawing.Point(139, 274);
+            this.edit_btn.Name = "edit_btn";
+            this.edit_btn.Size = new System.Drawing.Size(115, 46);
+            this.edit_btn.TabIndex = 8;
+            this.edit_btn.Text = "Edit";
+            this.edit_btn.UseVisualStyleBackColor = true;
+            // 
+            // signout_button
+            // 
+            this.signout_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.signout_button.Location = new System.Drawing.Point(299, 274);
+            this.signout_button.Name = "signout_button";
+            this.signout_button.Size = new System.Drawing.Size(116, 47);
+            this.signout_button.TabIndex = 9;
+            this.signout_button.Text = "Signout";
+            this.signout_button.UseVisualStyleBackColor = true;
+            // 
+            // User_Details
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(560, 418);
+            this.Controls.Add(this.signout_button);
+            this.Controls.Add(this.edit_btn);
+            this.Controls.Add(this.Output_City);
+            this.Controls.Add(this.Output_Phone);
+            this.Controls.Add(this.Output_Email);
+            this.Controls.Add(this.Output_Name);
+            this.Controls.Add(this.Label_City);
+            this.Controls.Add(this.Label_Phone);
+            this.Controls.Add(this.Label_Email);
+            this.Controls.Add(this.Label_Name);
+            this.Name = "User_Details";
+            this.Text = "User Details";
+            this.Load += new System.EventHandler(this.User_Details_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Label_Name;
+        private System.Windows.Forms.Label Label_Email;
+        private System.Windows.Forms.Label Label_Phone;
+        private System.Windows.Forms.Label Label_City;
+        private System.Windows.Forms.Label Output_Name;
+        private System.Windows.Forms.Label Output_Email;
+        private System.Windows.Forms.Label Output_Phone;
+        private System.Windows.Forms.Label Output_City;
+        private System.Windows.Forms.Button edit_btn;
+        private System.Windows.Forms.Button signout_button;
+    }
+}
